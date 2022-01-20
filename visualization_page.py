67914ca6255763caaaec1d1596246043f0e96737
Muhammad Iqbal Al-Haswan B <iqbal.haswan@gmail.com>
2022-01-20 20:42:13 +0800

    Update visualization_page.py

--- visualization_page.py
+++ visualization_page.py
@@ -4,9 +4,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import datetime
-
-from login_page import query
-
+import os
 
 def visualization_page():
     # html_temp = "<div class='tableauPlaceholder' id='viz1638766654954' style='position: relative'><noscript><a href=''><img alt='DStreamlit ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ba&#47;BansosRevisi&#47;DStreamlit&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='BansosRevisi&#47;DStreamlit' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ba&#47;BansosRevisi&#47;DStreamlit&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1638766654954');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='1827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='1827px';} else { vizElement.style.width='100%';vizElement.style.height='3327px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"
@@ -28,7 +26,7 @@ def visualization_page():
     # st.write("Total Penerima: ", all_bansos["KET_KK_NIK" == "KK AMAN"].sum())
     # st.write("Total Ditolak: ", )
 
-    # dbconnection_str = 'mysql+pymysql://'+['db_username']+':'+['db_password']+'@'+['db_host']+'/'+['db_name']
+    # dbconnection_str = 'mysql+pymysql://'+os.environ['db_username']+':'+os.environ['db_password']+'@'+os.environ['db_host']+'/'+os.environ['db_name']
     # engine = create_engine(dbconnection_str)
     # query = engine.execute('select bansos.nik, bansos.no_kk, bansos.nama, bansos.nik_capil, bansos.no_kk_capil, bansos.nama_lgkp_capil, bansos.status, bansos.kategori, bansos.opd_pengampu, bansos.tahap, bansos.alamat_capil, bansos.kelurahan_capil, bansos.kecamatan_capil, bansos.domisili, bansos.ket_nik, bansos.jenis_kelamin, bansos.ket_nama, bansos.ket_kk_nik, bansos.usia, bansos.label, bansos.date, kelurahan.latitude, kelurahan.longitude  from bansos left join kelurahan on bansos.kelurahan_capil = kelurahan.kelurahan_capil')
     # rows = query.fetchall()
