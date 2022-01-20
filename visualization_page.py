2726ff2061528671485a82d4270f529a7c00a72d
Muhammad Iqbal Al-Haswan B <iqbal.haswan@gmail.com>
2022-01-20 16:56:04 +0800

    Update visualization_page.py

--- visualization_page.py
+++ visualization_page.py
@@ -12,7 +12,7 @@ def visualization_page():
     # html_temp = "<div class='tableauPlaceholder' id='viz1638766654954' style='position: relative'><noscript><a href=''><img alt='DStreamlit ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ba&#47;BansosRevisi&#47;DStreamlit&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='BansosRevisi&#47;DStreamlit' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ba&#47;BansosRevisi&#47;DStreamlit&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1638766654954');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='1827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='1827px';} else { vizElement.style.width='100%';vizElement.style.height='3327px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"
     #components.html(html_temp, width=1000, height=1500)
     st.markdown("""
-        <iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/88cbb69d-8792-440d-b8d4-e232eca1c746/page/6Z7gC" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe width="1000" height="1500" src="https://datastudio.google.com/embed/reporting/88cbb69d-8792-440d-b8d4-e232eca1c746/page/6Z7gC" frameborder="0" style="border:0" allowfullscreen></iframe>
     """, unsafe_allow_html=True)
     
     # try:
@@ -238,13 +238,13 @@ def visualization_page():
 #     st.plotly_chart(figStatus, use_container_width=True)
 
 
-    # USIA DAN GENDER
-    dfUsia = df_selection[['USIA', 'JENIS_KELAMIN']]
-    dfUsia = dfUsia.value_counts().reset_index()
-    dfUsia.columns = ['USIA', 'JENIS KELAMIN', 'JUMLAH']
-    dfUsia = dfUsia.sort_values(by = 'USIA')
-    figUsia = px.line(dfUsia, x='USIA', y='JUMLAH', color='JENIS KELAMIN', symbol="JENIS KELAMIN", title='Persebaran Data berdasarkan Usia')
-    st.plotly_chart(figUsia, use_container_width=True)
+#     # USIA DAN GENDER
+#     dfUsia = df_selection[['USIA', 'JENIS_KELAMIN']]
+#     dfUsia = dfUsia.value_counts().reset_index()
+#     dfUsia.columns = ['USIA', 'JENIS KELAMIN', 'JUMLAH']
+#     dfUsia = dfUsia.sort_values(by = 'USIA')
+#     figUsia = px.line(dfUsia, x='USIA', y='JUMLAH', color='JENIS KELAMIN', symbol="JENIS KELAMIN", title='Persebaran Data berdasarkan Usia')
+#     st.plotly_chart(figUsia, use_container_width=True)
 
 
 
